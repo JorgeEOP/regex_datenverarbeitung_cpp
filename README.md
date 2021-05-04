835672a1ab8b8a86334d64c75ea17feac49d5421
Jorge Enrique Olivares Peña <jorge_jaeger@MacBook-Pro-von-Jorge.local>
2021-05-04 16:00:25 +0200

    V.0.0 Erledigt

--- README.md
+++ README.md
@@ -1,4 +1,5 @@
 Regex Datenverarbeitung
 
 Dieses Koden ist herstellt um mehr ueber c++ zu lernen.
+
 *Ziel: Ein Datei lesen -> Regulaere Ausdruck finden -> Datenverarbeitung
--- geo_opt_energie_plot.cpp
+++ geo_opt_energie_plot.cpp
@@ -8,8 +8,10 @@ using namespace std;
 int main() {
 	ifstream geo_opt_f("2-PorYPc-1stGEO-pos-1.xyz");
 	ofstream ergebnisse_f("Energies.dat");
-	string line;
-	regex energie("(\\s*)(i =)(\\s*[0-9]*,)\\s*(E =)\\s*([+-]?([0-9]*[.])?[0-9]+)");
+	std::string line;
+	std::regex energie("(\\s*)(i =)(\\s*[0-9]*,)\\s*(E =)\\s*([+-]?([0-9]*[.])?[0-9]+)");
+	std::string iter_num;
+	std::string iter_ene;
 	// (^|\n)Um die Anfang eines linie zu finden
 	// * : heisst Egal wie viele von diese Ausdruck
 	// \\s : heisst \ fuer C++ compilatoren, \s ein leeres Ausdruck
@@ -17,15 +19,19 @@ int main() {
 	if (geo_opt_f.is_open()) {
 	 while (getline(geo_opt_f,line)) {
 			if (regex_match(line, energie)){
-				cout << line << endl ;
-				ergebnisse_f << line << endl;
+				//iter_num = line.substr(0,line.find(","));
+				iter_num = line.substr(10,3);
+				iter_ene = line.substr(line.find("-"));
+
+				cout << iter_num << " " << iter_ene << endl;
+				ergebnisse_f << iter_num << "  " << iter_ene << endl;
 			}
 	}
 	geo_opt_f.close();
 	ergebnisse_f.close();
 	}
 
-	else cout << "Datei koennte nicht heruntergeladen" << endl;
+	else cout << "Datei koennte nicht herunterladen" << endl;
 
 	return 0;
 }
